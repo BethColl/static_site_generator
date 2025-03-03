f4cc418db82e25544070593e836932e318fecce2
Haal-ITae <BethLynn12100@gmail.com>
2025-03-03 12:52:44 -0600

    added textnode and updated main

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from textnode import *
+
+print("main.py is running")
+
+def main():
+	dummy_text_node = TextNode(("Dummy text"), TextType.LINK, "https://www.boot.dev")
+	print(repr(dummy_text_node))
+
+if __name__=="__main__":
+	main()
