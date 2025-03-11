802a76db236682a2517382a68bb543e9d884d007
Haal-ITae <BethLynn12100@gmail.com>
2025-03-11 12:51:43 -0500

    corrected htmlnode error and added defensive code to htmlnode checks

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -7,15 +7,19 @@ class HTMLNode:
 		self.props = props #dictionary of key-value pairs representing the attributes of the HTML tag
 
 	def to_html(self):
-		raise exception(NotImplementedError)
+		raise NotImplementedError
 
 	def props_to_html(self):
+		if not self.props:
+			return ""
 		html = ""
 		for prop in self.props:
 			html = html + (f' {prop}="{self.props[prop]}"')
 		return html
-        
+
 	def __eq__(self, other):
+		if not isinstance(other, HTMLNode):
+			return False
 		return self.tag == other.tag and self.value == other.value and self.children == other.children and self.props == other.props
 
 	def __repr__(self):
