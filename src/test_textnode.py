c534cb5cc2e2d17178a51a3c9c60f8737157c76e
Haal-ITae <BethLynn12100@gmail.com>
2025-03-16 20:12:23 -0500

    added text-html converter

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -17,17 +17,32 @@ class TestTextNode(unittest.TestCase):
 		node2 = TextNode("This is a link", TextType.LINK, "https://www.boot.dev/")
 		self.assertEqual(node, node2)
 	def test_links_noteq_type(self):
-		node = TextNode("This is not a link", TextType.NORMAL_TEXT)
+		node = TextNode("This is not a link", TextType.TEXT)
 		node2 = TextNode("This is a link", TextType.LINK, "https://www.boot.dev/")
 		self.assertNotEqual(node, node2)
 	def test_text_noteq(self):
-		node = TextNode("This is one text node", TextType.NORMAL_TEXT)
-		node2 = TextNode("This is another text node", TextType.NORMAL_TEXT)
+		node = TextNode("This is one text node", TextType.TEXT)
+		node2 = TextNode("This is another text node", TextType.TEXT)
 		self.assertNotEqual(node, node2)
 	def test_null_url_match(self):
 		node = TextNode("This is a None url", TextType.LINK, None)
 		node2 = TextNode("This is a None url", TextType.LINK)
 		self.assertEqual(node, node2)
+	def test_text(self):
+		node = TextNode("This is a text node", TextType.TEXT)
+		html_node = node.text_node_to_html_node()
+		self.assertEqual(html_node.tag, None)
+		self.assertEqual(html_node.value, "This is a text node")
+	def test_bold(self):
+		node = TextNode("This is bolded text", TextType.BOLD_TEXT)
+		html_node = node.text_node_to_html_node()
+		self.assertEqual(html_node.tag, 'b')
+		self.assertEqual(html_node.value, "This is bolded text")
+	def test_italic_difference(self):
+		node = TextNode("This is italicized text", TextType.ITALIC_TEXT)
+		html_node = node.text_node_to_html_node()
+		self.assertEqual(html_node.tag, 'i')
+		self.assertNotEqual(html_node.value, "This is italic text")
 
 if __name__ == "__main__":
 	unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
-	NORMAL_TEXT = "normal"
+	TEXT = "normal"
 	BOLD_TEXT = "bold"
 	ITALIC_TEXT = "italic"
 	CODE_TEXT = "code"
@@ -19,3 +20,20 @@ class TextNode:
 
 	def __repr__(self):
 		return (f"TextNode({self.text}, {self.text_type.value}, {self.url})")
+
+	def text_node_to_html_node(self):
+		if self.text_type not in TextType:
+			raise ValueError("Not valid Text Type.")
+		elif self.text_type == TextType.TEXT:
+			return LeafNode(None, self.text)
+		elif self.text_type == TextType.BOLD_TEXT:
+			return LeafNode('b', self.text)
+		elif self.text_type == TextType.ITALIC_TEXT:
+			return LeafNode('i', self.text)
+		elif self.text_type == TextType.CODE_TEXT:
+			return LeafNode('code', self.text)
+		elif self.text_type == TextType.LINK:
+			return LeafNode('a', self.text, self.url)
+		elif self.text_type == TextType.IMAGE:
+			return LeafNode('img', '', self.url)
+		else: raise ValueError("Text Type not recognized.")
